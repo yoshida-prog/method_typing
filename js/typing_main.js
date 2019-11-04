1d8c056bf81d8b80d939db11a75c7ce7d294ccf6
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2019-11-04 16:51:08 +0900

    commit

--- js/typing_main.js
+++ js/typing_main.js
@@ -3,19 +3,54 @@ var window_obj = window;
 
 //問題文と意味
 const WORDS = [
-  ['border-radius: 5px;', 'CSS: 要素の境界の外側の角を5px丸める'],
-  ['color: #ffffff;', 'CSS: 文字の色を#ffffffに変える'],
+  //HTML
   ['<input type="submit">', 'HTML: 送信ボタンを作成'],
-  ['Math.floor(Math.random()*10)', 'js: 0から9までの数字をランダムで取得'],
-  ['var i=0; i++;', 'js: 変数iに1足す'],
   ['<tr></tr>', 'HTML: 行'],
+  ['<table></table>', 'HTML: 表'],
+  ['<select></select>', 'HTML: セレクトボックス'],
+  ['<input type="radio">', 'HTML: ラジオボックス'],
+  //CSS
   ['margin: 0;', 'CSS: 要素のボーダーより外側の上下左右の余白を0にする'],
   ['padding: 10px;', 'CSS: 要素のボーダーより内側の上下左右の余白を10pxにする'],
+  ['border-radius: 5px;', 'CSS: 要素の境界の外側の角を5px丸める'],
+  ['color: #ffffff;', 'CSS: 文字の色を#ffffffに変える'],
+  ['font-weight: bold;', 'CSS: 文字を太文字にする'],
+  ['line-height: 150%;', 'CSS: 行の高さを150%に指定する'],
+  ['text-align: center;', 'CSS: ブロックコンテナ内の行の揃え位置・均等割付を中央に指定する'],
+  ['word-spacing: 2px;', 'CSS: 文字の間隔を2pxに指定する'],
+  ['min-height: 1080px;', 'CSS: 高さの最小値を1080pxに指定する'],
+  ['display: none;', 'CSS: 要素の表示形式をnoneにする'],
+  ['z-index: 0;', 'CSS: 重なりの順序を指定する'],
+  ['cursor: pointer;', 'CSS: リンクカーソルを指定'],
+  ['a:hover{background-color: #333;}', 'CSS: aタグにカーソルが乗った時、背景色を#333にする'],
+  //js
+  ['Math.floor(Math.random()*10)', 'js: 0から9までの数字をランダムで取得'],
+  ['var i=0; i++;', 'js: 変数iに1足す'],
+  ['var window_obj = window;', 'js: 変数window_objにwindowオブジェクトを格納'],
+  ['console.log();', 'js: 引数の値を出力テストする'],
+  //jQuery
   ['$().click()', 'jQuery: 対象をクリック時にイベント発火'],
   ['$(window).keydown()', 'jQuery: キーを押した時発火'],
-  ['empty()', 'PHP: 引数が空か調べる'],
-  ["define('NAME', VALUE)", "PHP: 定数定義('定数名', 値)"],
-  ['preg_match()', 'PHP: 正規表現によるマッチング']
+  ['$("p").show("slow");', 'jQuery: pタグを緩やかに表示状態にする'],
+  ['$(document).ready(function(){}', 'jQuery: DOMがロードされたタイミングで関数を実行'],
+  ['$(window).keydown(function(){}', 'jQuery: キーボードの何かのキーが押し込まれた際に関数を実行'],
+  ['$(window).keyup(function(){}', 'jQuery: キーボードのキーが押され、上がった際に関数を実行'],
+  ['$(p).text()', 'jQuery: 指定した要素が持つテキストノードを結合したものを返す'],
+  ['$(option).val()', 'jQuery: 指定した要素が持つvalue属性を返す'],
+  ['$("p").append("hello")', 'jQuery: 指定した要素の末尾に文字列のhelloを追加する'],
+  //PHP
+  ['empty();', 'PHP: 引数が空か調べる'],
+  ["define('NAME', VALUE);", "PHP: 定数定義('定数名', 値)"],
+  ['preg_match();', 'PHP: 正規表現によるマッチング'],
+  ['session_start();', 'PHP: セッションスタート'],
+  ['$box = array();', 'PHP: 配列定義'],
+  ['strlen($str)', 'PHP: 引数$strの長さを返す'],
+  ['$_POST[]', 'PHP: POST送信で来た値を取得'],
+  ['echo "str";', 'PHP: １つ以上の文字列を出力する'],
+  ['var_dump($str);', 'PHP: 構造化された引数の情報を返す'],
+  ['<?php ?>', 'PHP: HTML内でPHPを実行'],
+  ['require("test.php");', 'PHP: test.phpファイルを読み込む'],
+  ["unset($_SESSION['err_msg']);", 'PHP: 引数の値を破棄する']
 ];
 
 //押したキーの正誤をカウントする変数
